4af6adc50c29a411e2fc1b3323028816ec4a643f
151250121 <151250121@smail.nju.edu.cn>
2017-10-14 12:46:51 +0800

    <dev>:<add a simple version of navigator>

---
+++ Photour/src/components/Layout/Layout.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <navigator>
+    {{ msg }}
+  </navigator>
+
+</template>
+
+<script>
+  import Navigator from './Navigator'
+
+  export default {
+    name: '',
+    components: {
+      Navigator
+    },
+    data () {
+      return {
+        msg: 'test test test',
+      }
+    },
+    computed: {
+
+    }
+  }
+
+</script>
---
+++ Photour/src/components/Layout/Navigator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="body-wrapper">
+    <div class="nav-wrapper">
+      <slot>
+
+      </slot>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'navigator'
+  }
+</script>
+
+<style scoped>
+  @import "./Navigator.css";
+</style>
